b91b97fe98cad032ab17bf12324db8ed369d4ad6
Paul Andrews <paul.andrews@defra.gov.uk>
2021-03-06 09:13:01 +0000

    Messaging config

---
+++ app/config/messaging.js
@@ -0,0 +1,20 @@
+const sharedConfig = {
+  appInsights: require('applicationinsights'),
+  host: process.env.SERVICE_BUS_HOST,
+  password: process.env.SERVICE_BUS_PASSWORD,
+  username: process.env.SERVICE_BUS_USER,
+  useCredentialChain: process.env.NODE_ENV === 'production'
+}
+
+const msgTypePrefix = 'uk.gov.ffc.grants'
+
+module.exports = {
+  fixmeQueue: {
+    address: process.env.FIXME_QUEUE_ADDRESS,
+    type: FIXME,
+    ...sharedConfig
+  },
+  fixmeMsgType: `${msgTypePrefix}.fixme.fixme`,
+  msgSrc: 'ffc-grants-desirability-scoring'
+}
+
