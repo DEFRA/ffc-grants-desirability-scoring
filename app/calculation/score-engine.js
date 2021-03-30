df60ec16972e7fde8b0f2d069978c7bee85952a7
govTechSatish <80264942+govTechSatish@users.noreply.github.com>
2021-03-30 09:08:47 +0100

    Feature/scoring logic (#7)
    * Added Scoring Engine Logic
    * Create new fake message per test
    
    Co-authored-by: Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>

---
+++ app/calculation/score-engine.js
@@ -0,0 +1,238 @@
+
+const { first } = require('lodash')
+const bandHigh = 'High'
+const bandLow = 'Low'
+const bandMedium = 'Medium'
+
+class ScoreEngine {
+  constructor (desirabilityAssessment) {
+    this.scoringData = require('./score-data.json')
+    this.desirabilityAssessment = desirabilityAssessment
+  }
+
+  getScore () {
+    // calculate each question result
+    this.desirabilityAssessment.desirability.questions
+      .map((qanswer, index, allQanswers) => calculate(qanswer, this.scoringData.desirability, allQanswers))
+    const maxScore =
+      this.scoringData.desirability.questions
+        .reduce((total, question) => question.maxScore + total, 0)
+    const noShowResultQuestions =
+      this.scoringData.desirability.questions
+        .filter(question => question.showResult === false)
+    const actualScore = this.desirabilityAssessment.desirability.questions
+      .filter(question =>
+        noShowResultQuestions
+          .every(noShow => noShow.key !== question.key))
+      .reduce(
+        (total, question) =>
+          question.rating.score + total, 0)
+    this.desirabilityAssessment.desirability.overallRating.score = actualScore
+    const bandScore = (actualScore / maxScore) * 100
+
+    // remove noShowResult questions
+    this.desirabilityAssessment.desirability.questions = this.desirabilityAssessment.desirability.questions.filter(question => noShowResultQuestions.every(noShow => noShow.key !== question.key))
+
+    // get overall rating band
+    this.desirabilityAssessment.desirability.overallRating.band = getOverAllRatingBand(bandScore, this.scoringData.desirability)
+    return this.desirabilityAssessment
+  }
+}
+
+function getOverAllRatingBand (bandScore, sectionScoringData) {
+  if (bandScore >=
+    first(
+      sectionScoringData.overallRatingScoreData
+        .filter(x => x.name === bandHigh)).value) {
+    return bandHigh
+  }
+  if (bandScore <
+    first(
+      sectionScoringData.overallRatingScoreData
+        .filter(x => x.name === bandLow)).value) {
+    return bandLow
+  }
+  return bandMedium
+}
+
+function calculate (qanswer, sectionScoringData, allQanswers) {
+  // get question
+  const question = first(
+    sectionScoringData.questions
+      .filter(q => q.key === qanswer.key))
+  const dependentQuestionRatingScore = []
+  if (question.dependentQuestions) {
+    const answers = allQanswers.filter(x => question.dependentQuestions.some(d => d === x.key))
+    answers.forEach(dqa => {
+      dependentQuestionRatingScore.push(dqa.rating.score)
+    })
+  }
+  qanswer.rating = calculateQScore(question, qanswer.answers, dependentQuestionRatingScore)
+  return qanswer
+}
+
+function calculateQScore (question, answers, dependentQuestionRatingScore) {
+  let result = new ScoreResult('', '')
+  switch (String(question.scoreType).toLowerCase()) {
+    case 'dualsumweightband':
+      result = dualSumWeightBand(question, answers)
+      break
+
+    case 'answervalnoband':
+      result = answerValNoBand(question, answers)
+      break
+
+    case 'dualquestionhectorscore':
+      result = dualQuestionHectorScore(question, answers, dependentQuestionRatingScore)
+      break
+
+    case 'dualavgmatrix':
+      result = dualAvgMatrix(question, answers)
+      break
+
+    case 'dualsum':
+      result = dualSum(question, answers)
+      break
+
+    case 'boolweightscore':
+      result = boolWeightScore(question, answers)
+      break
+  }
+  return result
+}
+
+// Q14
+function dualSumWeightBand (question, answers) {
+  let score = question.answer
+    .filter(itemX =>
+      first(answers).input.some(itemY => itemY.key === itemX.key))
+    .reduce((total, answer) => answer.weight + total, 0) * question.weight
+
+  const scoreBand = score / question.maxScore
+
+  score = Math.round(score)
+
+  let band = bandMedium
+  if (scoreBand <= first(
+    question.scoreData.scoreBand
+      .filter(x => x.name === bandLow)).value) { band = bandLow }
+  if (scoreBand >= first(
+    question.scoreData.scoreBand
+      .filter(x => x.name === bandHigh)).value) { band = bandHigh }
+
+  return new ScoreResult(score, band)
+}
+
+// Q15
+function answerValNoBand (question, answers) {
+  const score = first(question.answer
+    .filter(x =>
+      first(answers).input.some(y => y.key === x.key)
+    )).weight
+  return new ScoreResult(score, null)
+}
+
+// Q16
+function dualQuestionHectorScore (question, answers, dependentQuestionRatingScore) {
+  // Q15 Score
+  const q15Score = first(dependentQuestionRatingScore)
+  // 16a
+  const q16aAnsVal = first(
+    first(answers.filter(x => x.key === `${question.key}a`)
+    ).input).value
+
+  const q16bAnsVal = first(
+    first(answers.filter(x => x.key === `${question.key}b`)).input).value
+
+  const hector = q16aAnsVal === q16bAnsVal ? q16bAnsVal : (q16aAnsVal + q16bAnsVal) / 2
+
+  const bandBoundary = first(question.scoreData.scoreMatrix.filter(e => e.crop === q15Score))
+  let band = bandMedium
+  if (hector <= bandBoundary.low) { band = bandLow }
+  if (hector >= bandBoundary.high) { band = bandHigh }
+
+  const score = first(
+    question.scoreData.scoreBand
+      .filter(x => x.name === band)).value * question.weight
+  return new ScoreResult(score, band)
+}
+
+// Q20
+function boolWeightScore (question, answers) {
+  const score = first(
+    question.answer
+      .filter(x =>
+        first(answers).input
+          .some(y => y.key === x.key))).weight * question.weight
+  let band = bandHigh
+  if (score <= first(
+    question.scoreData.scoreBand
+      .filter(r => r.name === bandLow)).value) { band = bandLow }
+  return new ScoreResult(score, band)
+}
+
+// Q18/17
+function dualAvgMatrix (question, answers) {
+  const asIsAnswers =
+    question.answer
+      .filter(x => first(
+        answers
+          .filter(z => z.key === `${question.key}a`)).input
+        .some(y => y.key === x.key))
+  const toBeAnswers =
+  question.answer.filter(x => first(
+    answers.filter(z => z.key === `${question.key}b`)).input
+    .some(y => y.key === x.key))
+
+  const asIsAverage = Math.round(
+    asIsAnswers.reduce((total, next) => total + next.weight, 0) / asIsAnswers.length)
+  const tobeAverage = Math.round(
+    toBeAnswers.reduce((total, next) => total + next.weight, 0) / toBeAnswers.length)
+
+  const matrixVal = first(
+    question.scoreData.scoreMatrix
+      .filter(r => r.id === String(asIsAverage)))[String(tobeAverage)]
+
+  const score = matrixVal * question.weight
+  const scoreBand = matrixVal / question.maxScore
+
+  let band = bandMedium
+  if (scoreBand <= first(
+    question.scoreData.scoreBand
+      .filter(r => r.name === bandLow)).value) { band = bandLow }
+  if (scoreBand >= first(
+    question.scoreData.scoreBand
+      .filter(r => r.name === bandHigh)).value) { band = bandHigh }
+
+  return new ScoreResult(score, band)
+}
+
+// Q19
+function dualSum (question, answers) {
+  const score =
+  question.answer
+    .filter(itemX => first(answers).input
+      .some(itemY => itemY.key === itemX.key))
+    .reduce((total, answer) => answer.weight + total, 0) * question.weight
+
+  const scoreBand = score / question.maxScore
+
+  let band = bandMedium
+  if (scoreBand <= first(
+    question.scoreData.scoreBand
+      .filter(r => r.name === bandLow)).value) { band = bandLow }
+  if (scoreBand >= first(
+    question.scoreData.scoreBand
+      .filter(r => r.name === bandHigh)).value) { band = bandHigh }
+
+  return new ScoreResult(score, band)
+}
+
+class ScoreResult {
+  constructor (score, band) {
+    this.score = score
+    this.band = band
+  }
+}
+
+module.exports = ScoreEngine
