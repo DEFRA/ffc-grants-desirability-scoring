ef01c0166b0aebe699b45261db40eed3e61c19fd
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-06 14:32:32 +0100

    Add correlationId to messages

--- app/messaging/calculate-score.js
+++ app/messaging/calculate-score.js
@@ -2,14 +2,14 @@ const { sendScoreCalculated } = require('./senders')
 const ScoreEngine = require('../../app/calculation/score-engine')
 module.exports = async function (msg, calculateScoreReceiver) {
   try {
-    const { body } = msg
+    const { body, correlationId } = msg
     console.log('Received message:')
     console.log(body)
 
     const scoreEngine = new ScoreEngine(body)
     const scoreResult = scoreEngine.getScore()
 
-    await sendScoreCalculated(scoreResult)
+    await sendScoreCalculated(scoreResult, correlationId)
 
     await calculateScoreReceiver.completeMessage(msg)
   } catch (err) {
