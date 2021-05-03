7beba40313ad9913e25d0ff04612b1a8c1b91cb6
Satish Chatap <satish.chatap@capgemini.com>
2021-05-03 19:01:56 +0100

    removed unnecessary validation.

--- app/config/server.js
+++ app/config/server.js
@@ -1,14 +1,3 @@
-const Joi = require('joi')
-
-// Define config schema
-const schema = Joi.object({
-  protectiveMonitoringUrl: Joi.string().allow(''),
-  appInsights: {
-    key: Joi.string(),
-    role: Joi.string().default('ffc-grants-desirability-scoring')
-  }
-})
-
 // Build config
 const config = {
   protectiveMonitoringUrl: process.env.PROTECTIVE_MONITORING_URL,
@@ -18,14 +7,4 @@ const config = {
   }
 }
 
-// Validate config
-const result = schema.validate(config, {
-  abortEarly: false
-})
-
-// Throw if config is invalid
-if (result.error) {
-  throw new Error(`The server config is invalid. ${result.error.message}`)
-}
-
-module.exports = result.value
+module.exports = config
