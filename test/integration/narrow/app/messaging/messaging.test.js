400efd402e441450142f73e3472d04e9df499d11
Satish Chatap <satish.chatap@capgemini.com>
2021-05-04 10:16:00 +0100

    more test

--- test/integration/narrow/app/messaging/messaging.test.js
+++ test/integration/narrow/app/messaging/messaging.test.js
@@ -1,5 +1,4 @@
 jest.mock('ffc-messaging')
-jest.mock('../../../../../app/services/protective-monitoring-service')
 const ffcMessaging = require('ffc-messaging')
 ffcMessaging.MessageSender = jest.fn().mockImplementation(() => {
   return {
@@ -13,6 +12,14 @@ ffcMessaging.MessageReceiver = jest.fn().mockImplementation((queue, updateAction
     subscribe: jest.fn()
   }
 })
+const mockSendEvent = jest.fn()
+jest.mock('ffc-protective-monitoring', () => {
+  return {
+    PublishEvent: jest.fn().mockImplementation(() => {
+      return { sendEvent: mockSendEvent }
+    })
+  }
+})
 describe('messaging tests', () => {
   test('Senders Should be defined', () => {
     const senders = require('../../../../../app/messaging/senders')
@@ -21,6 +28,7 @@ describe('messaging tests', () => {
   test('Senders sendScoreCalculated Should not throw error', async () => {
     const senders = require('../../../../../app/messaging/senders')
     await expect(senders.sendScoreCalculated('', '')).resolves.not.toThrow()
+    expect(mockSendEvent).toHaveBeenCalledTimes(1)
   })
   test('Receiver Should be defined', () => {
     const receivers = require('../../../../../app/messaging/receivers')
