6525bddcbb6357dbf9677d02f2e1c745a9f3317f
zavies97 <zacsdavies@sky.com>
2022-07-05 13:27:51 +0100

    Updated standardised cost in backend

--- app/messaging/standardised-costs.js
+++ app/messaging/standardised-costs.js
@@ -1,4 +1,4 @@
-const { grantMsgSender } = require('./senders')
+const { sendMessage } = require('./index')
 const appInsights = require('../services/app-insights')
 const ScoreEngine = require('../../app/calculation/score-engine')
 const scoreDataRepository = require('../services/score-repository')
@@ -6,14 +6,17 @@ const msgCfg = require('../config/messaging')
 const msgTypePrefix = 'uk.gov.ffc.grants'
 module.exports = async function (msg, grantReciever) {
   console.log(msg.applicationProperties.type,'i am in tryyyyy LLLLLLLLLL')
+
+  // this should process response and send back to queue
   try {
     const { body, correlationId, applicationProperties } = msg
+    // what is correlationId?
    
     const msgType = applicationProperties.type.replace(msgTypePrefix, '')
     let grantType = null
     let senderMsgType = null
-    if (msgType === '.slurry.desirability.calculate') {
-      senderMsgType = msgCfg.desirabilitySlurryMsgType
+    if (msgType === '.fetch.app.request') {
+      senderMsgType = msgCfg.applicationResponseMsgType
       grantType = 'Slurry Infrastructure Grant'
     }
     const grantData = await scoreDataRepository.getScoreData(grantType)
@@ -22,7 +25,7 @@ module.exports = async function (msg, grantReciever) {
       const scoreResult = scoreEngine.getScore()
       console.log('Score result:')
       console.log(JSON.stringify(scoreResult, null, 2))
-      await grantMsgSender(scoreResult, correlationId, senderMsgType)
+      await sendMessage(scoreResult, msgCfg.applicationResponseMsgType, msgCfg.applicationResponseQueue, { sessionId }) // sessionId comes from received message
 
       await grantReciever.completeMessage(msg)
     } else {
