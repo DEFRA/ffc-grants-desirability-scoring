d3a00528b6ec158e06febfb8fe49de173ab53509
yunusfirat <yunusfirat44@gmail.com>
2022-11-18 14:51:12 +0000

    send and receive message tests

--- test/integration/narrow/app/messaging/app/index.test.js
+++ test/integration/narrow/app/messaging/app/index.test.js
@@ -1,5 +1,5 @@
 const { sendResponseToSession } = require('../../../../../../app/messaging/application')
-const { costResponseQueue, fetchCostResponseMsgType, fetchScoreResponseMsgType, scoreResponseQueue } = require('../../../../../../app/config/messaging.js')
+const { costResponseQueue, fetchCostResponseMsgType, fetchScoreResponseMsgType, scoreResponseQueue, scoreWaterResponseQueue, fetchWaterScoreResponseMsgType } = require('../../../../../../app/config/messaging.js')
 
 jest.mock('../../../../../../app/messaging')
 const { sendMessage } = require('../../../../../../app/messaging')
@@ -19,7 +19,7 @@ describe('application messaging tests', () => {
     expect(sendMessage).toHaveBeenCalledWith({}, fetchCostResponseMsgType, costResponseQueue, { sessionId })
   })
 
-  test('getApplication sends and receives message with different msgType', async () => {
+  test('getApplication sends and receives message with scoring msgType', async () => {
 
     await sendResponseToSession({}, sessionId, '.fetch.score.request')
 
@@ -27,5 +27,19 @@ describe('application messaging tests', () => {
     expect(sendMessage).toHaveBeenCalledWith({}, fetchScoreResponseMsgType, scoreResponseQueue, { sessionId })
   })
 
-  
+  test('getApplication sends and receives message with water msgType', async () => {
+
+    await sendResponseToSession({}, sessionId, '.fetch.water.score.request') 
+
+    expect(sendMessage).toHaveBeenCalledTimes(1)
+    expect(sendMessage).toHaveBeenCalledWith({}, fetchWaterScoreResponseMsgType, scoreWaterResponseQueue, { sessionId })
+  })
+
+  test('get application send and receives messages with default or no message type', async () => {
+
+    await sendResponseToSession({}, sessionId, 'hello world!')
+
+    expect(sendMessage).toHaveBeenCalledTimes(1)
+    expect(sendMessage).toHaveBeenCalledWith({}, fetchCostResponseMsgType, costResponseQueue, { sessionId })
+  })
 })
