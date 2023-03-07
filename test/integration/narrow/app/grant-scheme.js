6e78cb9cda8df949db0ffdd1b8a08d3c91ca4310
diya2214 <madihakhn@yahoo.com>
2023-03-07 15:42:02 +0000

    score fix

--- test/integration/narrow/app/grant-scheme.js
+++ test/integration/narrow/app/grant-scheme.js
@@ -65,8 +65,8 @@ module.exports = {
                 title: 'Under your current irrigation, where does the water come from [primary?]?',
                 input: [
                   {
-                    key: 'water-source-a-A1',
-                    value: 'Peak-flow surface water'
+                    key: 'water-source-a-A4',
+                    value: 'Summer water surface abstraction'
                   },
                   {
                     key: 'water-source-a-A3',
