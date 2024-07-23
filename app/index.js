f135bb1b7f645057f3ab766af6cf9520d28f83a6
Andrew-Folga-EE <173707519+Andrew-Folga@users.noreply.github.com>
2024-07-23 09:30:47 +0100

    Clean up code related to unused calculate score queue (#97)
    * Clean up code related to unused calculate score queue
    
    * Bump version
    
    * Revert test changes - done as part of pull 96
    
    * Rename the test

--- app/index.js
+++ app/index.js
@@ -2,9 +2,6 @@ const server = require('./server')
 const messageService = require('./messaging/service')
 
 const init = async () => {
-  const calculateScoreAction = require('./messaging/calculate-score')
-  require('./messaging/receivers').startCalculateScoreReceiver(calculateScoreAction)
-
   await messageService.start()
   await server.start()
   console.log('Server running on %s', server.info.uri)
